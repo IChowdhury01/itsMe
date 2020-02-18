0544ee186430079139e64f05c5759c76dcf3447c
Ivan Chowdhury <moaggaimc@gmail.com>
2020-02-18 03:16:37 -0500

    Created basic UI + notification for home page

--- App/src/main/java/sp2020/attachablesmartlock/MainActivity.java
+++ App/src/main/java/sp2020/attachablesmartlock/MainActivity.java
@@ -3,6 +3,8 @@ package sp2020.attachablesmartlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    /**
+     *  Function that is called whenever the LOCK/UNLOCK button is clicked.
+     *  Creates a Toast object (on-screen notification) and shows it on the screen.
+      */
+    public void onLockButtonTap(View v) {
+        Toast myToast = Toast.makeText(getApplicationContext(), "Your device is now locked!", Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
