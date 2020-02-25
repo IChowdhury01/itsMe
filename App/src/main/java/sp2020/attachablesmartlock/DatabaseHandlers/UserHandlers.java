c4ac4f12222c749564b391a13b0b8022785303ce
Ivan Chowdhury <moaggaimc@gmail.com>
2020-02-25 17:55:45 -0500

    commented out

--- App/src/main/java/sp2020/attachablesmartlock/DatabaseHandlers/UserHandlers.java
+++ App/src/main/java/sp2020/attachablesmartlock/DatabaseHandlers/UserHandlers.java
@@ -1,35 +1,35 @@
-package sp2020.attachablesmartlock.DatabaseHandlers;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import sp2020.attachablesmartlock.Users.User;
-
-public class UserHandlers {
-
-    // Needs try catch
-    public static void initDatabase() {
-
-
-
-    }
-
-    public static User getUser(int userID) throws SQLException {
-
-    }
-
-    // Needs try catch
-    public static Boolean createUser (String inputName, int inputLockID, String uploadedFileName) {
-
-    }
-
-    public static Boolean createUser (User user) {
-
-    }
-
-    public static ArrayList<User> getUserList() throws SQLException {
-
-    }
-
-
-}
+//package sp2020.attachablesmartlock.DatabaseHandlers;
+//
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+//import sp2020.attachablesmartlock.Users.User;
+//
+//public class UserHandlers {
+//
+//    // Needs try catch
+//    public static void initDatabase() {
+//
+//
+//
+//    }
+//
+//    public static User getUser(int userID) throws SQLException {
+//
+//    }
+//
+//    // Needs try catch
+//    public static Boolean createUser (String inputName, int inputLockID, String uploadedFileName) {
+//
+//    }
+//
+//    public static Boolean createUser (User user) {
+//
+//    }
+//
+//    public static ArrayList<User> getUserList() throws SQLException {
+//
+//    }
+//
+//
+//}
