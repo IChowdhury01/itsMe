cd6c9a27f42f1a8e126d91e9c4cfa81f55e2d358
hyungseokyoon <47166576+hyungseokyoon@users.noreply.github.com>
2020-05-02 13:47:03 -0400

    new files for the app is uploaded

--- App/src/main/java/sp2020/attachablesmartlock/MainActivity.java
+++ App/src/main/java/sp2020/attachablesmartlock/MainActivity.java
@@ -1,198 +1,142 @@
-package sp2020.attachablesmartlock;
+package com.example.rushi.leds;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.widget.Button;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
-
-import sp2020.attachablesmartlock.Locks.Lock;
-import sp2020.attachablesmartlock.Timers.BoundedCounter;
-import sp2020.attachablesmartlock.Timers.Timer;
-import sp2020.attachablesmartlock.Users.Friend;
-import sp2020.attachablesmartlock.Users.User;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPaired,btnBluetooth;
+    private ListView deviceList;
+    private TextView textView;
+
+    private boolean bluetoothOnFlag = false;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_NAME = "device_name";
+    public static String EXTRA_ADDRESS = "device_address";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_device_list);
 
-        runTests();
+        btnPaired = (Button)findViewById(R.id.show_paired_button);
+        btnBluetooth = (Button)findViewById(R.id.bluetooth_on);
+        deviceList = (ListView)findViewById(R.id.device_list_view);
+        textView = (TextView)findViewById(R.id.textView);
+
+        //if the device has bluetooth
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnBluetooth.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                bluetoothButtonClick();
+            }
+        });
+
+
+        btnPaired.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(bluetoothOnFlag || bluetoothAdapter.isEnabled())
+                    pairedDevicesList();
+                else
+                    msg("Please Turn Bluetooth On");
+            }
+        });
     }
 
-    /**
-     * Function that is called whenever the home screen LOCK button is clicked.
-     * Creates a Toast object (on-screen notification) and shows it on the screen.
-     */
-    public void onLockButtonTap(View v) {
-        Toast myToast = Toast.makeText(getApplicationContext(), "Your device is now locked!", Toast.LENGTH_LONG);
-        myToast.show();
+    private void bluetoothButtonClick()
+    {
+        if(bluetoothAdapter == null)
+        {
+            //Show a message that the device has no bluetooth adapter
+            msg("Bluetooth Device Not Available");
+            //finish apk
+            finish();
+        }
+        else if( !bluetoothAdapter.isEnabled() )
+        {
+            //Ask to the user turn the bluetooth on
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon,1);
+            bluetoothOnFlag = true;
+        }
+        else
+        {
+            msg("Bluetooth is already on!");
+            bluetoothOnFlag = true;
+        }
     }
 
+    private void pairedDevicesList()
+    {
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        ArrayList arrayList = new ArrayList();
 
+        if (pairedDevices.size()>0)
+        {
+            textView.setText("Paired Devices:");
+            for(BluetoothDevice bt : pairedDevices)
+            {
+                arrayList.add(bt.getName().trim() + "\n" + bt.getAddress().trim());//Get the device's name and the address
+            }
+        }
+        else
+        {
+            msg("No Paired Bluetooth Devices Found.");
+        }
 
-    /**
-     * Function that is called whenever the home screen "Friends" button is clicked.
-     * It will redirect the user to his friends list page.
-     */
-    public void onFriendButtonTap(View v) {
-        Toast myToast = Toast.makeText(getApplicationContext(), "Friend's list not implemented yet.", Toast.LENGTH_LONG);
-        myToast.show();
-    }
-
-    public void onBluetoothTap(View v) {
-        Intent intent = new Intent(this, Bluetooth.class);
-        startActivity(intent);
-    }
-
-    public void onLogoTap(View v) {
-        Intent intent = new Intent(this, showinfo.class);
-        startActivity(intent);
-        //Toast myToast = Toast.makeText(getApplicationContext(), "Friend's list not implemented yet.", Toast.LENGTH_LONG);
-        //myToast.show();
-    }
-
-    /**
-     * Function that is called whenever the user clicks his profile picture on the home screen.
-     * It will redirect the user to his User profile page.
-     */
-    public void onProfilePicTap(View v) {
-
-        Intent intent = new Intent(this, Userprofile.class);
-        startActivity(intent);
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, arrayList);
+        deviceList.setAdapter(arrayAdapter);
+        deviceList.setOnItemClickListener(myListClickListener);//Method called when the device from the list is clicked
 
     }
 
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick (AdapterView<?> av, View v, int arg2, long arg3)
+        {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String name = info.substring(0,info.indexOf("\n"));
+            String address = info.substring(info.length() - 17);
 
-    /**
-     * This function runs all code-testing methods.
-     */
-    public static void runTests() {
-        Log.d("myTest", "This will print if the app is successfully run.");
+            // Make an intent to start next activity.
+            Intent nextActivity = new Intent(MainActivity.this, Motor_Control.class);
 
-        Random randomizer = new Random();   // If RNG is used for some tests
+            //Change the activity.
+            nextActivity.putExtra(EXTRA_ADDRESS, address);
+            nextActivity.putExtra(EXTRA_NAME, name);
+            startActivity(nextActivity);
+        }
+    };
 
-        // Comment out tests after they're working.
-//        testLock(randomizer);
-//        testCounter();
-//        testTimer();
-//        testFriend();
-//        testUsers();
+    // efficient way to call Toast
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
     }
 
-
-    // Testing methods
-
-    public static void testLock(Random randomizer) {
-        // Test lock class.
-        Lock myLock = new Lock(randomizer.nextInt(9999999));
-        Log.d("myTestClass L", myLock.toString());
-
-        Log.d("myTestClass L", "Lock ID is " + myLock.getLockID() + " and lock state is " + myLock.isLocked());
-
-        Log.d("myTestClass L", myLock.lockStateToString());
-
-        myLock.switchLockState();
-
-        Log.d("myTestClass L", myLock.toString());
-    }
-
-    public static void testCounter() {
-        BoundedCounter myCounter = new BoundedCounter(59);
-
-        Log.d("myTestClass C", myCounter.toString());
-        Log.d("myTestClass C", "Counter's current value is " + myCounter.getValue());
-
-        myCounter.setValue(24);
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.setValue(60);
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.setValue(-2);
-        Log.d("myTestClass C", myCounter.toString());
-
-        myCounter.setValue(1);
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.decrease();
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.decrease();
-        Log.d("myTestClass C", myCounter.toString());
-
-        myCounter.setValue(58);
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.increase();
-        Log.d("myTestClass C", myCounter.toString());
-        myCounter.increase();
-        Log.d("myTestClass C", myCounter.toString());
-    }
-
-    public static void testTimer() {
-        Timer myTimer = new Timer(0, 0, 0);
-        Log.d("myTestClass Timer", myTimer.toString());
-
-        myTimer.setTime(23, 59, 59);
-        Log.d("myTestClass Timer", myTimer.toString());
-        Log.d("myTestClass Timer", "Seconds: " + myTimer.getTimeInSeconds());
-
-        myTimer.tickDown();
-        Log.d("myTestClass Timer", myTimer.toString());
-
-        myTimer.setTime(23, 59, 0);
-        myTimer.tickDown();
-        Log.d("myTestClass Timer", myTimer.toString());
-
-        myTimer.setTime(23, 0, 0);
-        myTimer.tickDown();
-        Log.d("myTestClass Timer", myTimer.toString());
-
-        myTimer.setTime(0, 0, 0);
-        myTimer.tickDown();
-        Log.d("myTestClass Timer", myTimer.toString());
-    }
-
-    public static void testFriend() {
-        User user1 = new User("User1", 00001);
-        User user2 = new User("User2", 00002);
-
-        Friend addedUser = new Friend(user1);   // Creates a friend object representing user2.
-        Friend addedUserv2 = new Friend(user2);  // method 2
-
-        addedUserv2.grantAccess(1, 23, 5);
-        Log.d("myTestClass Friend", addedUser.toString());
-        Log.d("myTestClass Friend", addedUserv2.toString());
-
-        addedUserv2.revokeAccess();
-        Log.d("myTestClass Friend", addedUserv2.toString());
-    }
-
-    public static void testUsers() {
-        // Create users
-        User user1 = new User("User1", 00001);
-        User user2 = new User("User2", 00002, "myPic.jpg");
-        User user3 = new User("User3", 00003);
-
-        Log.d("myTestClass User", user1.toString());
-        Log.d("myTestClass User", user2.toString());
-
-        // Add friends
-        Log.d("myTestClass User", user1.getUsersFriendsList().toString());
-        user1.getUsersFriendsList().addFriend(user2);
-        user1.getUsersFriendsList().addFriend(user3);
-        Log.d("myTestClass User", user1.getUsersFriendsList().toString());
-
-        // Remove friends
-        user1.getUsersFriendsList().removeFriend(user3);
-        Log.d("myTestClass User", user1.getUsersFriendsList().toString());
-
-        // Clear list
-        user1.getUsersFriendsList().clearFriends();
-        Log.d("myTestClass User", user1.getUsersFriendsList().toString());
-    }
 }
+
+
